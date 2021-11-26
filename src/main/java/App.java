580bacae690650698ab5eac43b3fb556d4f7bf51
alex23140 <xteamx23@gmail.com>
2021-11-26 17:18:32 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println(bean == bean2);
+
+        Cat bean3 = (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean3.getMassage());
+        Cat bean4 = (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean4.getMassage());
+        System.out.println(bean3==bean4);
     }
 }
